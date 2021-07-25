76e719054882db46d918bf3aff0e4391f801bbc7
Jenselens <jenselens27@gmail.com>
2021-07-25 11:45:40 +0530

    Message

---
+++ src/org/emp/Details.java
@@ -0,0 +1,19 @@
+package org.emp;
+
+public class Details {
+private void name() {
+System.out.println("Jenifer");
+}
+private void age() {
+System.out.println("25");
+}
+private void salary() {
+System.out.println("1243543");
+}
+public static void main(String[] args) {
+	Details d = new Details();
+	d.name();
+	d.age();
+	d.salary();
+}
+}
